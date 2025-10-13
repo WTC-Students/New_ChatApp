c9c5ba786a93a52923a00219289348c2923edfd4
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-10-13 10:30:06 +0200

    Signin1Activity.kt

--- app/src/main/java/com/example/new_chatapp/Signin1Activity.kt
+++ app/src/main/java/com/example/new_chatapp/Signin1Activity.kt
@@ -1,4 +1,39 @@
-package com.example.new_chatapp
+package com.example.chatapp
 
-class Signin1Activity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class SignIn1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_1)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+            }
+        })
+
+        findViewById<View?>(R.id.emailButton).setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@SignIn1Activity, SignIn2Activity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        })
+
+        val signUpLink = findViewById<TextView>(R.id.signUpLink)
+        signUpLink.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@SignIn1Activity, SignUp1Activity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        })
+    }
 }
