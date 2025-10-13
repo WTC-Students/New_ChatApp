658a516d215cd7cde3705b4277eeefcaf2ba00a1
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-10-13 10:29:01 +0200

    Onboardng2Activity.kt

--- app/src/main/java/com/example/new_chatapp/Onboardng2Activity.kt
+++ app/src/main/java/com/example/new_chatapp/Onboardng2Activity.kt
@@ -1,4 +1,33 @@
-package com.example.new_chatapp
+package com.example.chatapp
 
-class Onboardng2Activity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Onboarding2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_2)
+
+        findViewById<View?>(R.id.getStartedBtn).setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@Onboarding2Activity, SignIn1Activity::class.java)
+                startActivity(intent)
+                finish()
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        })
+
+        val skipText = findViewById<TextView>(R.id.skipText)
+        skipText.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@Onboarding2Activity, SignIn1Activity::class.java)
+                startActivity(intent)
+                finish()
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        })
+    }
 }
